2769cff6b59f65934c48cf2e7ebef8b6c3e29221
AmarHadj <Planteramartin@gmail.com>
2024-03-26 18:32:36 -0400

    ajout du rebond

--- Project/Scripts/Joueur.gd
+++ Project/Scripts/Joueur.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 @onready var bounce_raycasts = $BounceRayCasts
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
+const BOUNCE_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,7 +16,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -26,4 +27,17 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	_check_bounce(delta)
 	move_and_slide()
+	
+func _check_bounce(delta):
+	if velocity.y > 0:
+		for raycast in bounce_raycasts.get_children():
+			raycast.target_position = Vector2.DOWN * velocity * delta + Vector2.DOWN
+			raycast.force_raycast_update()
+			if raycast.is_colliding() && raycast.get_collision_normal() == Vector2.UP:
+				velocity.y = (raycast.get_collision_point() - raycast.global_position - Vector2.DOWN).y / delta
+				raycast.get_collider().entity.call_deferred("be_bounced_upon", self)
+				break
+func bounce (bounce_velocity = BOUNCE_VELOCITY):
+	velocity.y = bounce_velocity
--- Project/Scripts/Monstre.gd
+++ Project/Scripts/Monstre.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func be_bounced_upon(bouncer):
+	if bouncer.has_method("bounce"):
+		bouncer.bounce()
+		queue_free()
+		
