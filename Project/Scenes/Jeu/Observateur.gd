8925c93f04cb7bbdd979b7d770d7ed58385de420
AmarHadj <Planteramartin@gmail.com>
2024-05-09 19:58:57 -0400

    Menu pause complété
    Il faut juste faire en sorte que esc fonctionne de nouveau dans le menu pause

--- Project/Scenes/Jeu/Observateur.gd
+++ Project/Scenes/Jeu/Observateur.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @export var player: CharacterBody2D
-@export var canvas: CanvasModulate
+@export var canvasGameOver: CanvasModulate
 @export var RetryButton: Button
 @export var MainMenuButton: Button
 @export var QuitButton: Button
@@ -9,38 +9,40 @@ extends Node2D
 @export var MainMenuButtonPause: Button
 @export var QuitButtonPause: Button
 
+var isPaused: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		RetryButton.visible = false
-		MainMenuButton.visible = false
-		QuitButton.visible = false
-		canvas.visible = false
-		ReturnButtonPause.visible = false
-		MainMenuButtonPause.visible = false
-		QuitButtonPause.visible = false
-		canvasPause.visible = false
+		isPaused = false
+		makeButtonAppear(canvasGameOver, RetryButton, MainMenuButton, QuitButton, false)
+		makeButtonAppear(canvasPause, ReturnButtonPause, QuitButtonPause, MainMenuButtonPause, false)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player == null :
-		GameOver()
+		makeButtonAppear(canvasGameOver, RetryButton, MainMenuButton, QuitButton, true)
 	
-	if Input.is_action_just_pressed("esc") :
-		ReturnButtonPause.visible = true
-		MainMenuButtonPause.visible = true
-		QuitButtonPause.visible = true
-		canvasPause.visible = true
+	if Input.is_action_just_pressed("esc") && isPaused == false && player != null:
+		makeButtonAppear(canvasPause, ReturnButtonPause, QuitButtonPause, MainMenuButtonPause, true)
+		isPaused = true
+		get_tree().paused = true
+	elif Input.is_action_just_pressed("esc") && isPaused == true && player != null:
+		makeButtonAppear(canvasPause, ReturnButtonPause, QuitButtonPause, MainMenuButtonPause, false)
+		isPaused = false
+		get_tree().paused = false
 
 func _on_retry_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Jeu/Jeu.tscn")
 
 func _on_return_pressed():
-	pass # Replace with function body.
+	get_tree().paused = false
+	makeButtonAppear(canvasPause, ReturnButtonPause, QuitButtonPause, MainMenuButtonPause, false)
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Jeu/Menu.tscn")
 	
 func _on_pause_main_menu_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Jeu/Menu.tscn")
 
 func _on_quit_pressed():
@@ -49,8 +51,9 @@ func _on_quit_pressed():
 func _on_pause_quit_pressed():
 	get_tree().quit()
 
-func GameOver():
-	RetryButton.visible = true
-	MainMenuButton.visible = true
-	QuitButton.visible = true
-	canvas.visible = true
+
+func makeButtonAppear(canvas: CanvasModulate, button1: Button, button2: Button, button3: Button, isVisible: bool) :
+	button1.visible = isVisible
+	button2.visible = isVisible
+	button3.visible = isVisible
+	canvas.visible = isVisible
